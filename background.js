3ae1712aba9bf90c04d3ef0b710287d5fa85dd41
ngocanhnckh <ngocanhnckh@gmail.com>
2019-11-10 00:26:56 +0700

    first commit

---
+++ background.js
@@ -0,0 +1,44 @@
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getStatus")
+      sendResponse({status: localStorage['link']});
+    else
+      sendResponse({}); // snub them.
+});
+chrome.tabs.onUpdated.addListener(function() {
+    onWindowLoad();
+    });
+    
+    setInterval(function() { 
+        var a = JSON.parse(localStorage.getItem("link"));
+        print(a);
+        for (let index = 0, len = a.length; index < len; ++index) {
+           if (a[index].status==0){
+                a[index].status = check(a[index].url); 
+           }       
+        }
+    },10000);   
+     
+    function onWindowLoad() {
+        chrome.storage.sync.get(['foo', 'bar'], function(items) {
+            alert('Settings retrieved', items);
+          });
+    chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+    }, function() {
+    if (chrome.extension.lastError) {
+    htmlcontent = 'There was an error injecting script : \n' +     chrome.extension.lastError.message;
+    }
+    });
+    }
+    
+
+
+    chrome.extension.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+    htmlcontent = request.source;
+    //alert(htmlcontent)
+    //call whatever function from here on so that htmlcontent will have the selected tab's content.
+    }
+    
+    });
---
+++ contentscript.js
@@ -0,0 +1,10 @@
+chrome.runtime.sendMessage({method: "getStatus"}, function(response) {
+    alert(response.status);
+  });
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('script.js');
+(document.head||document.documentElement).appendChild(s);
+s.onload = function() {
+    s.parentNode.removeChild(s);
+};
+
